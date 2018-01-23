b7e8a34d58e7f5fa9fc0678881e4ac420f7cc829
Marcelo Banhos <marcelobanhos@MacBook-Air-de-Marcelo.local>
2018-01-23 21:33:55 -0200

    Commit inicial

---
+++ Carreiras/DAO/CulturaDAO.swift
@@ -0,0 +1,13 @@
+//
+//  CulturaDAO.swift
+//  Carreiras
+//
+//  Created by Marcelo Banhos on 23/01/18.
+//  Copyright © 2018 Marcelo Banhos. All rights reserved.
+//
+
+import UIKit
+
+class CulturaDAO: NSObject {
+
+}
---
+++ Carreiras/Model/Cultura.swift
@@ -0,0 +1,13 @@
+//
+//  Cultura.swift
+//  Carreiras
+//
+//  Created by Marcelo Banhos on 23/01/18.
+//  Copyright © 2018 Marcelo Banhos. All rights reserved.
+//
+
+import UIKit
+
+class Cultura: NSObject {
+
+}
